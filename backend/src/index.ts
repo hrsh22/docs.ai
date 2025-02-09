43435a33b426615ec615d8f04de20f07355b4e92
Harsh Gupta <harsh.gupta2211@gmail.com>
2025-02-09 16:08:36 +0530

    implement backend for base / basename and openai

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+// Main entry point - Initializes and starts the express server
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import someRoute from './routes/someRoute';
+import basenameRoute from './routes/getBasename';
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(bodyParser.json())
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Enable CORS for all origins
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+// Route handling
+app.use('/api', someRoute);
+app.use('/api', basenameRoute);
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
